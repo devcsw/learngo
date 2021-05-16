d8177e0d469571c1d97eb649cba77c939744f8e2
devcsw <mbso520@gmail.com>
2021-05-16 20:47:59 +0900

    #1.5 for, range args

--- main.go
+++ main.go
@@ -1,34 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-//리턴 값 먼저 설정 하는법 (naked return)
-func lenAndUpper(name string) (lenght int, uppercase string) {
-	//함수가 끝난후 실행 defer
-	defer fmt.Println("I'm done")
-	lenght = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+
+	// for i := 0; i < len(numbers); i++ {
+	//		total += number
+	// }
 }
 
-//여러 argument 받아들이는법
-//배열로 받아짐
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
+// for index, number := range numbers {
+// 	fmt.Println(index, number)
+// }
+// return 1
 
 func main() {
-	fmt.Println(lenAndUpper("dd"))
-
-	totalLenght, upperName := lenAndUpper("choi")
-	//defer로 끝났을때마다 실행
-	fmt.Println(totalLenght, upperName)
-	//여러값 리턴 무시하기 언더바(_)
-	totalLenght2, _ := lenAndUpper("park")
-	fmt.Println(totalLenght2)
-
-	repeatMe("1", "2", "3", "4", "5")
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
