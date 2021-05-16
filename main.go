be67361e254a9b2d74cea1c39d2867c279dbf7ad
devcsw <mbso520@gmail.com>
2021-05-16 20:27:12 +0900

    #1.4 Functions part Two

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-//여러값 반환
-func lenAndUpper(name string) (int, string, int) {
-	return len(name), strings.ToUpper(name), 1
+//리턴 값 먼저 설정 하는법 (naked return)
+func lenAndUpper(name string) (lenght int, uppercase string) {
+	//함수가 끝난후 실행 defer
+	defer fmt.Println("I'm done")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 //여러 argument 받아들이는법
@@ -21,14 +21,14 @@ func repeatMe(words ...string) {
 }
 
 func main() {
-	fmt.Println(multiply(2, 2))
 	fmt.Println(lenAndUpper("dd"))
 
-	totalLenght, upperName, num := lenAndUpper("choi")
-	fmt.Println(totalLenght, upperName, num)
+	totalLenght, upperName := lenAndUpper("choi")
+	//defer로 끝났을때마다 실행
+	fmt.Println(totalLenght, upperName)
 	//여러값 리턴 무시하기 언더바(_)
-	totalLenght2, upperName2, _ := lenAndUpper("park")
-	fmt.Println(totalLenght2, upperName2)
+	totalLenght2, _ := lenAndUpper("park")
+	fmt.Println(totalLenght2)
 
 	repeatMe("1", "2", "3", "4", "5")
 }
